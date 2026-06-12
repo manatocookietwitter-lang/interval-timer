84fd8727461c520c2dcf77afa168d707d7aefddc
manato <manatocookietwitter@gmail.com>
2026-06-12 12:48:18 +0900

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'interval-timer-pwa-v4';
+const ASSETS = ['./','./index.html','./manifest.json','./sw.js','./icons/timer-192.png','./icons/timer-512.png','./icons/flame-192.png','./icons/flame-512.png','./icons/bolt-192.png','./icons/bolt-512.png','./icons/dumbbell-192.png','./icons/dumbbell-512.png','./icons/rings-192.png','./icons/rings-512.png'];
+self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy)); return response; }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then((cached) => { if (cached) return cached; return fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html')); }));
+});
